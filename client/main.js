9dea297dd87ddac16f9044b30dd0c1b27824406a
Hack Reactor Student <student@hackreactor.com>
2020-01-03 17:42:48 -0700

    ServerSide Mostly Working Get Issues on Client

--- client/main.js
+++ client/main.js
@@ -11,14 +11,19 @@ $(document).ready(function() {
   });
 
   function getQuote(){
-
     //YOUR CODE HERE, Add a GET request
-
+        // need to figure out what Data needs to send as the data for req
+    $.get('http://localhost:3000/quotes', {}, function(quote) {
+      console.log('inside return from get request:', quote)
+      $(document).getElementById('quote').append(quote)
+    }, 'text');
   }
 
   function addQuote(quote){
-    
     //YOUR CODE HERE, Add a POST request
-
+    $.post('http://localhost:3000/quotes', {data: quote}, function(quote) {
+      console.log('inside return from post request:', quote)
+      //$(document).getElementById('quote').append(quote)
+    }, 'text');
   }
 });
